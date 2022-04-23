cf8e8eaa676b1f61466b8705f1e923836434ed51
unknown <joaomgoncalvesbr@gmail.com>
2022-04-22 22:29:37 -0300

    setting up token jwt

--- app.js
+++ app.js
@@ -20,9 +20,68 @@ app.get("/", (req, res) => {
 //Register User
 app.post("/auth/register", async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
+  const userExists = await User.findOne({ email: email });
 
   if (!name)
     return res.status(422).json({ message: "O campo nome é obrigatório!" });
+  if (!email)
+    return res.status(422).json({ message: "O campo e-mail é obrigatório!" });
+  if (!password)
+    return res.status(422).json({ message: "O campo password é obrigatório!" });
+  if (password !== confirmPassword)
+    return res.status(422).json({ message: "As senhas não são iguais!" });
+
+  if (userExists)
+    return res
+      .status(422)
+      .json({ message: "O e-mail informado já foi cadastrado!" });
+
+  //create password
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  try {
+    await user.save();
+    res.status(201).json({ message: "Usuário criado com sucesso!" });
+  } catch (error) {
+    return res.status(500).json({ message: "Algo deu errado." });
+  }
+});
+
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  console.log("user", user);
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!email)
+    return res.status(422).json({ message: "O campo e-mail é obrigatório!" });
+  if (!password)
+    return res.status(422).json({ message: "O campo password é obrigatório!" });
+
+  if (!user)
+    return res.status(404).json({ message: "Usuário não encontrado!" });
+
+  if (!checkPassword)
+    return res.status(404).json({ message: "Senha inválida!" });
+
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign({ id: user._id }, secret);
+    return res.status(200).json({
+      message: "Usuário autenticado com sucesso!",
+      access_token: token,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Algo deu errado." });
+  }
 });
 
 mongoose
